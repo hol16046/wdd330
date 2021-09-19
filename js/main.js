26cf6a2b899ddb6b7cdb77b9278d9e65d24a00ed
hol16046 <hol16046@byui.edu>
2021-09-18 23:52:06 -0600

    week 1

---
+++ js/main.js
@@ -0,0 +1,36 @@
+// Dynamic Table of Contents
+const links = [
+    {
+        label:'Week 1',
+        url: 'week1'
+    }
+];
+
+links.forEach(tableOfContents);
+function tableOfContents(link) {
+    document.getElementById('toc').innerHTML += "<li>" + "<a href='" + link.url + "'>" + link.label + "</li>";
+}
+
+// Get current year for copyright information.
+let d = new Date()
+var year = d.getFullYear();
+document.getElementById("copyrightdate").textContent = year;
+
+// Get last modified date of current document.
+
+var lastmod = document.lastModified;
+document.getElementById("modifieddate").textContent = lastmod;
+
+// Page Visit Count (LocalStorage)
+if (localStorage) {
+    let visits = localStorage.getItem('visits');
+    if (visits == null) {
+        visits = 1;
+    }
+    if (visits == 1) {
+        document.getElementById('visitCount').innerHTML = "This is your first visit to this page.";
+    } else {
+        document.getElementById('visitCount').innerHTML = `You have visited this page ${visits} times.`;
+    }
+    localStorage.setItem('visits', Number(visits) + 1);
+}
