46ea5cc7ae4f95acb76fbcc9f7abcb4aee9fe416
hol16046 <hol16046@byui.edu>
2021-10-23 23:51:07 -0600

    midterm final

--- week06/midterm/todos.js
+++ week06/midterm/todos.js
@@ -14,18 +14,25 @@ export default class ToDos {
     getAllTasks() {
         toDoList = this.storage.readToDoList('listStorage');
         return toDoList;
-
     }
 
     getTaskByName(content) {
         return this.getAllTasks().find(task => task.content === content);
     }
 
+    getTaskByid(id) {
+		console.log(id);
+		console.log(this.getAllTasks());
+        return this.getAllTasks().find(task => task.id === id);
+    }
+
     showToDoList() {
         this.listParent.innerHTML = '';
         renderToDoList(this.listParent, this.getAllTasks());
-        renderForm(this.getAllTasks());
-        this.addToDoListener(this.getAllTasks());
+        renderForm();
+		renderFilters();
+        this.addToDoListener();
+		this.addToDoListenerManip();
     }
 
     addToDo(newTask) {
@@ -33,38 +40,44 @@ export default class ToDos {
         if (storedList != null) {
             toDoList = storedList;
         } else {
-            toDoList = new Array();
-        }
+			toDoList = new Array();
+		}
         console.log('add task');
         console.log(toDoList);
-        toDoList.push({id: new Date(),
+        toDoList.push(
+		{
+			id: new Date(),
             content: newTask,
             completed: false
         });
         this.storage.storeToDoList(toDoList);
+		console.log(toDoList);
         
     }
 
-    completeTask(content) {
-        const completedTask = this.getTaskByName(content);
+    completeTask(id) {
+        const completedTask = this.getTaskByid(id);
         if (completedTask.completed === false) {
             completedTask.completed = true;
         } else if (completedTask.completed === true) {
             completedTask.completed = false;
         };
+		//completedTask.completed = !completedTask.completed;
         this.storage.storeToDoList(toDoList);
         console.log(toDoList);
         return completedTask.completed;
     }
 
-    removeTask(taskElement, taskContent) {
+    removeTask(taskElement, taskid) {
         let list = this.storage.readToDoList('listStorage');
-        const removeMe = this.getTaskByName(taskContent);
-        console.log(removeMe);
-        const removeMeIndex = list.map(function(removeMe) { return removeMe.content; }).indexOf(taskContent);
-        list.splice(removeMeIndex, 1);
-        taskElement.remove();
-        this.storage.storeToDoList(list);
+        const removeMe = this.getTaskByid(taskid);
+        const removeMeIndex = list.map(function(removeMe) { return removeMe.id; }).indexOf(taskid);
+		if (removeMeIndex != -1)
+		{
+			list.splice(removeMeIndex, 1);
+			taskElement.remove();
+			this.storage.storeToDoList(list);
+		}
         console.log(list);
     }
 
@@ -98,37 +111,63 @@ export default class ToDos {
                 this.removeTask(e.currentTarget.parentElement, e.currentTarget.parentElement.dataset.name);
             });
         });
+	}
+	addToDoListenerManip() {
+        // Filter Task
+        const toDoFilters = Array.from(document.getElementsByClassName('toDoFilters'));
+        toDoFilters.forEach(child => {
+            child.addEventListener('click', e => {
+                //this.removeTask(e.currentTarget.parentElement, e.currentTarget.parentElement.dataset.name);
+				console.log(e.target.id);
+				if(e.target.id == "activeTasks")
+				{
+					this.listParent.innerHTML = '';
+					renderToDoList(this.listParent, this.filterUnfinishedTasks());
+					this.addToDoListener();
+				}
+				if(e.target.id == "completedTasks")
+				{
+					this.listParent.innerHTML = '';
+					renderToDoList(this.listParent, this.filterCompleteTasks());
+					this.addToDoListener();
+				}
+				if(e.target.id == "allTasks")
+				{
+					this.listParent.innerHTML = '';
+					renderToDoList(this.listParent, this.getAllTasks());
+					this.addToDoListener();
+				}
+            });
+        });
 
         // Add Task Form
         const newTaskContent = document.getElementById('newTask');
-        console.log(document.getElementById('addButton'));
+        console.log(newTaskContent.value);
         document.getElementById('addButton').addEventListener('click', (event) => {
             this.addToDo(newTaskContent.value);
-        console.log(toDoList);
         });
     }
-     
-    
-
 }
 
 //End of ToDos Class
 
 function renderToDoList(parent, tasks) {
+	console.log(parent)
+	//this.listParent.innerHTML = '';
     if (tasks != null) {
-
         const checkBox = document.querySelector('.completedBox');
         tasks.forEach(task => {
+			//console.log(renderOneTask(task));
             parent.appendChild(renderOneTask(task));
-        });
-    }
+		});
+	}
 }
-
 function renderOneTask(task) {
     const item = document.createElement('li');
     item.classList.add('task');
-    item.setAttribute('data-name', task.content);
-    item.innerHTML = `<div class="completedBox` + (task.completed ? `completed` : ``) + `"></div>
+    //item.setAttribute('data-name', task.content);
+    item.setAttribute('data-name', task.id);
+    item.innerHTML = `<div class="completedBox` + (task.completed ? ` completed` : `` )+ `"></div>
         <p>${task.content}</p>
         <div class="delete">
             <p>X</p>
@@ -136,10 +175,19 @@ function renderOneTask(task) {
     return item;
 }
 
-function renderForm(list) {
+function renderForm() {
     const form = document.createElement('form');
     form.classList.add('form');
     form.innerHTML = `<textarea name="newTask" id="newTask" placeholder="Enter your task here"></textarea>
         <button type="submit" id="addButton">+</button>`;
+    document.getElementById('container').appendChild(form);
+}
+
+function renderFilters() {
+    const form = document.createElement('form2');
+    form.classList.add('form2');
+    form.innerHTML = `<button id="allTasks" class="toDoFilters">All</button>
+	<button id="activeTasks" class="toDoFilters">Active</button>
+	<button id="completedTasks" class="toDoFilters">Completed</button>`;
     document.getElementById('container').appendChild(form);
 }
